bba1f2d0871e8780c549ab19eed2c90acff6f67e
masaru <massaru129@gmail.com>
2014-06-08 15:33:44 +0900

    プロジェクトの編集、一覧、削除まで実装

--- team_manager/app/controllers/projects_controller.rb
+++ team_manager/app/controllers/projects_controller.rb
@@ -1,11 +1,15 @@
 class ProjectsController < ApplicationController
   def new
+    @project = Project.new
   end
 
   def create
     @project = Project.new(project_params)	
-    @project.save
-    redirect_to @project
+    if @project.save
+      redirect_to @project
+	else
+	  render 'new'
+	end
   end
 
   def index
@@ -13,10 +17,26 @@ class ProjectsController < ApplicationController
   end
 
   def show
-  @project = Project.find(params[:id])
+    @project = Project.find(params[:id])
   end
 
-  def list
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+	@project.destroy
+	redirect_to projects_path
   end
 
   private
--- team_manager/app/models/project.rb
+++ team_manager/app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ActiveRecord::Base
+    validates :title, presence: true
+	validates :description, presence: true
+	#validates :project_manager_id, presence: true
+
 end
