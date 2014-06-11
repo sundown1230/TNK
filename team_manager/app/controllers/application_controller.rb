d0e9b4fbada79cabaa9835ecd48019db69ee355b
masaru <massaru129@gmail.com>
2014-06-11 17:33:21 +0900

    sign_up時のビューを変更

--- team_manager/app/controllers/application_controller.rb
+++ team_manager/app/controllers/application_controller.rb
@@ -4,8 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :set_locale
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def set_locale
 	I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def configure_permitted_parameters
+	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email, :generation, :description) }
   end
 end
