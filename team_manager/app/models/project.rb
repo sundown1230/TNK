cb7266da7f2202af0b8f3a755b059157e6b9f69f
massaru129 <massaru129@gmail.com>
2014-06-30 04:25:42 +0900

    Revert "Fix relation project report"

--- team_manager/app/models/project.rb
+++ team_manager/app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-    has_many :reports, dependent: :destroy
+    has_many :reports
     validates :title, presence: true
 	validates :description, presence: true
 	#validates :project_manager_id, presence: true
