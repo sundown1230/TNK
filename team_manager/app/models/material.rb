fb51d0fc290942d12b3f525e6ab0b515c0eb9ce0
masaru <massaru129@gmail.com>
2014-06-30 06:06:28 +0900

    modelにリレーションを記述

--- team_manager/app/models/material.rb
+++ team_manager/app/models/material.rb
@@ -1,4 +1,5 @@
 class Material < ActiveRecord::Base
+    has_many :users, through: :materials_users
     validates :title, presence:true
 	validates :text, presence:true
 end
--- team_manager/app/models/user.rb
+++ team_manager/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :materials through: :materials_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
