5b7d651a2ea46759542ec4943892771135ddf579
masaru <massaru129@gmail.com>
2014-07-30 19:23:45 +0900

    user一覧に権限を表示するように変更

--- team_manager/app/controllers/application_controller.rb
+++ team_manager/app/controllers/application_controller.rb
@@ -3,15 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_action :set_locale,:set_user
+  before_action :set_locale
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def set_locale
 	I18n.locale = params[:locale] || I18n.default_locale
-  end
-
-  def set_user
-    @user = User.first
   end
 
   def configure_permitted_parameters
