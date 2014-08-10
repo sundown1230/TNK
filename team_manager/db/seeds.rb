8d5769259fcc7f95f6961459187b267987af126d
masaru <massaru129@gmail.com>
2014-08-11 07:11:10 +0900

    プロジェクトのサンプルデータを作成

--- team_manager/db/seeds.rb
+++ team_manager/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# サンプルプロジェクトを挿入
+require 'date'
+Project.create(title: 'プロジェクト1', project_manager_id: 1, starts_at: Date.today.to_s(:db), ends_at: Date.today.to_s(:db), description: "サンプルプロジェクト1", status: 0, created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+Project.create(title: 'プロジェクト2', project_manager_id: 2, starts_at: Date.today.to_s(:db), ends_at: Date.today.to_s(:db), description: "サンプルプロジェクト2", status: 0, created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+Project.create(title: 'プロジェクト3', project_manager_id: 3, starts_at: Date.today.to_s(:db), ends_at: Date.today.to_s(:db), description: "サンプルプロジェクト3", status: 1, created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+Project.create(title: 'プロジェクト4', project_manager_id: 4, starts_at: Date.today.to_s(:db), ends_at: Date.today.to_s(:db), description: "サンプルプロジェクト4", status: 1, created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+Project.create(title: 'プロジェクト5', project_manager_id: 5, starts_at: Date.today.to_s(:db), ends_at: Date.today.to_s(:db), description: "サンプルプロジェクト5", status: 2, created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+
+
