d83276a0989009c627fc9f4091fa3746a09ca5c9
masaru <massaru129@gmail.com>
2014-09-04 04:05:42 +0900

    tasksコントローラーにクリエイトとデリートを追加 #41
    reportsの丸パクリ

--- team_manager/app/controllers/tasks_controller.rb
+++ team_manager/app/controllers/tasks_controller.rb
@@ -1,2 +1,19 @@
 class TasksController < ApplicationController
+  def create
+    @project = Project.find(params[:project_id])
+    @tasks = @project.tasks.create(task_params)
+    redirect_to project_path(@project)
+  end
+
+  def destroy
+    @project = Project.find(params[:project_id])
+    @report = @project.tasks.find(params[:id])
+    @report.destroy
+    redirect_to project_path(@project)
+  end
+ 
+  private
+    def task_params
+      params.require(:task).permit(:title, :project_id, :content, :due)
+    end
 end
