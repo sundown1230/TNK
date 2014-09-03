f030392d2e81227527c181ca32b585c716a9c7a8
masaru <massaru129@gmail.com>
2014-09-04 04:10:34 +0900

    taskとprojectにリレーションを追加 #41

--- team_manager/app/models/project.rb
+++ team_manager/app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
     has_many :reports, dependent: :destroy
+	has_many :tasks, dependent: :destroy
     validates :title, presence: true
 	validates :description, presence: true
 	#validates :project_manager_id, presence: true
