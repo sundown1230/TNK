674dabb9d398606281f72b2de314a1da112c1c5f
masaru <massaru129@gmail.com>
2014-09-10 07:07:07 +0900

    reportsコントローラーの編集を一旦コミット（完全ではない） #41

--- team_manager/app/controllers/reports_controller.rb
+++ team_manager/app/controllers/reports_controller.rb
@@ -1,10 +1,40 @@
 class ReportsController < ApplicationController
+  def new
+    @project = Project.find(params[:project_id])
+    @report = @project.reports.build
+  end
+
   def create
     @project = Project.find(params[:project_id])
     @report = @project.reports.create(report_params)
     redirect_to project_path(@project)
   end
 
+  def index
+    @project = Project.find(params[:project_id])
+    @reports = @project.reports.find(:all)
+  end
+
+  def show
+    @project = Project.find(params[:project_id])
+    @report = @project.reports.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:project_id])
+    @report = @project.reports.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:project_id])
+    @report = @project.reports.find(params[:id])
+    if @report.update(report_params)
+      redirect_to @report
+    else
+      render 'edit'
+    end
+  end
+ 
   def destroy
     @project = Project.find(params[:project_id])
     @report = @project.reports.find(params[:id])
@@ -14,6 +44,6 @@ class ReportsController < ApplicationController
  
   private
     def report_params
-      params.require(:report).permit(:title, :user_id, :text)
+      params.require(:report).permit(:id, :title, :user_id, :text)
     end
 end
