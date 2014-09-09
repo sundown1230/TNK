656c24f9d2ae91c9065c8350e796f0814a8ee39b
masaru <massaru129@gmail.com>
2014-09-10 07:05:39 +0900

    tasksコントローラーをaddし忘れていた #41

--- team_manager/app/controllers/tasks_controller.rb
+++ team_manager/app/controllers/tasks_controller.rb
@@ -1,8 +1,18 @@
 class TasksController < ApplicationController
+  def new
+    @project = Project.find(params[:project_id])
+	@task = @project.tasks.build
+  end
+
   def create
     @project = Project.find(params[:project_id])
     @tasks = @project.tasks.create(task_params)
     redirect_to project_path(@project)
+  end
+
+  def index
+    @project = Project.find(params[:project_id])
+	@tasks = @project.tasks.all
   end
 
   def destroy
