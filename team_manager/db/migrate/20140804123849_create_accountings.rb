565efe6a9f56077d67f921eac03c4a90446fc522
SUNDOWN1230 <hiruma.hh@gmail.com>
2014-09-12 22:30:29 +0900

    accountings-databaseのnullを不可に

--- team_manager/db/migrate/20140804123849_create_accountings.rb
+++ team_manager/db/migrate/20140804123849_create_accountings.rb
@@ -1,11 +1,11 @@
 class CreateAccountings < ActiveRecord::Migration
   def change
     create_table :accountings do |t|
-      t.string :title
-      t.string :name
-      t.text :purpose
-      t.date :date
-      t.integer :status
+      t.string :title, null:false
+      t.string :name, null:false
+      t.text :purpose, null:false
+      t.date :date, null:false
+      t.integer :status, null:false
       t.timestamps
     end
   end
