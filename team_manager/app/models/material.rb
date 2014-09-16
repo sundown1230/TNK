b56e7704de4586d74065c23f50ddf11d2cc04872
massaru129 <massaru129@gmail.com>
2014-09-17 06:12:59 +0900

    Revert "reportとuserのリレーション追加"

--- team_manager/app/models/material.rb
+++ team_manager/app/models/material.rb
@@ -2,7 +2,6 @@ class Material < ActiveRecord::Base
 	has_many :material_users
     has_many :users, through: :material_users
 	accepts_nested_attributes_for :material_users
-    
-	validates :title, presence:true
+    validates :title, presence:true
 	validates :text, presence:true
 end
