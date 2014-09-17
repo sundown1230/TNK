55f1150707a458f7e33ff0b333417ac77b8f022c
masaru <massaru129@gmail.com>
2014-09-18 03:15:48 +0900

    リレーションが保存されない誤りを修正 #44
    fields_forの第二引数もきちんと指定してやる必要がある

--- team_manager/app/models/project.rb
+++ team_manager/app/models/project.rb
@@ -1,8 +1,10 @@
 class Project < ActiveRecord::Base
     has_many :reports, dependent: :destroy
 	has_many :tasks, dependent: :destroy
+
 	has_many :projects_users
 	has_many :users, through: :projects_users
+	accepts_nested_attributes_for :projects_users
 
     validates :title, presence: true
 	validates :description, presence: true
