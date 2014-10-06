e50d41222ab220fb12f37bc21c8675cf7601b45b
masaru <massaru129@gmail.com>
2014-10-06 20:02:10 +0900

    ファイルアップロード用にモデルを修正

--- team_manager/app/models/material.rb
+++ team_manager/app/models/material.rb
@@ -3,5 +3,6 @@ class Material < ActiveRecord::Base
     has_many :users, through: :materials_users
 	accepts_nested_attributes_for :materials_users
     validates :title, presence:true
-	validates :text, presence:true
+	validates :filename, presence:true
+	validates :filetype, presence:true
 end
