a006160305735e0f1003a6b91e298a88e1dc3f4b
SUNDOWN1230 <hiruma.hh@gmail.com>
2014-10-15 23:46:15 +0900

    accountings_users_relation

--- team_manager/app/models/accounting.rb
+++ team_manager/app/models/accounting.rb
@@ -1,9 +1,9 @@
 class Accounting < ActiveRecord::Base
   validates :title, presence: true
-  validates :name, presence: true
   validates :purpose, presence: true
   validates :application_date, presence: true  
   validates :status, presence:true
   has_many :images, as: :imageable, autosave: true, dependent: :destroy
   accepts_nested_attributes_for :images
+  belongs_to :user
 end
--- team_manager/app/models/user.rb
+++ team_manager/app/models/user.rb
@@ -11,6 +11,8 @@ class User < ActiveRecord::Base
   has_many :projects_users, dependent: :destroy
   has_many :projects, through: :projects_users
 
+  has_many :accountings, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
